68da6d5f35a68bac139e5b56bee0488da7cada43
MagicMustache <milo.gianinazzi@gmail.com>
2021-03-31 10:34:07 +0200

    java to kotlin

---
+++ app/src/main/java/ch/friender/MainActivity.kt
@@ -0,0 +1,19 @@
+package ch.friender
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/ch/friender/Map.kt
@@ -0,0 +1,145 @@
+package ch.friender
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.maps.Style.OnStyleLoaded
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Map.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
+    private var permissionsManager: PermissionsManager? = null
+    private var mapboxMap: MapboxMap? = null
+    private var mapView: MapView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(requireActivity(), getString(R.string.mapbox_access_token))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        mapView = view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this@Map.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            enableLocationComponent(style)
+            if (mapboxMap.locationComponent.lastKnownLocation != null) {
+                val position = CameraPosition.Builder()
+                        .target(LatLng(mapboxMap.locationComponent.lastKnownLocation.getLatitude(), mapboxMap.locationComponent.lastKnownLocation.getLongitude()))
+                        .zoom(14.0)
+                        .tilt(20.0)
+                        .build()
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000)
+            }
+        }
+    }
+
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(requireActivity())) {
+
+            // Get an instance of the component
+            val locationComponent = mapboxMap.getLocationComponent()
+
+            // Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(requireActivity(), loadedMapStyle).build())
+
+            // Enable to make component visible
+            locationComponent.isLocationComponentEnabled = true
+
+            // Set the component's camera mode
+            locationComponent.cameraMode = CameraMode.TRACKING
+
+            // Set the component's render mode
+            locationComponent.renderMode = RenderMode.COMPASS
+            locationComponent.cameraMode = CameraMode.TRACKING_COMPASS
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(requireActivity())
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String?>?) {
+        Toast.makeText(activity, "test1", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            mapboxMap.getStyle(OnStyleLoaded { style -> enableLocationComponent(style) })
+        } else {
+            Toast.makeText(requireActivity(), "test2", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    companion object {
+        fun newInstance(): Map? {
+            return Map()
+        }
+    }
+}
