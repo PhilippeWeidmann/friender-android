feb06e02d462ba1cda90aaa642fbcda26ae46929
MagicMustache <milo.gianinazzi@gmail.com>
2021-04-04 00:49:03 +0200

    added location tracking (without play services)

--- app/src/main/java/ch/friender/MainActivity.kt
+++ app/src/main/java/ch/friender/MainActivity.kt
@@ -1,18 +1,23 @@
 package ch.friender
 
-import android.content.SharedPreferences
+
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import im.delight.android.location.SimpleLocation
 
 class MainActivity : FragmentActivity() {
+
+    private var location: SimpleLocation? = null
+    var lat:Double = 0.0
+    var long:Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,15 +25,16 @@ class MainActivity : FragmentActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
 
+        //get ID
         val sharedPreferences = getPreferences(MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         val url = "http://10.0.2.2:1337/user/register"
         val requestQueue = Volley.newRequestQueue(this)
-        if(sharedPreferences.getString("id","")?.isEmpty() == true){
-            val getId = JsonObjectRequest(Request.Method.POST,url,null, { res ->
+        if(sharedPreferences.getString("id", "")?.isEmpty() == true){
+            val getId = JsonObjectRequest(Request.Method.POST, url, null, { res ->
                 val id = res.get("data").toString()
-                Log.d("ID",id)
-                editor.putString("id",id)
+                Log.d("ID", id)
+                editor.putString("id", id)
                 editor.apply()
             }, { error ->
                 Log.e("error on getting id", error.toString())
@@ -36,8 +42,38 @@ class MainActivity : FragmentActivity() {
             requestQueue.add(getId)
         }
         else{
-            Log.d("already an id",""+sharedPreferences.getString("id",""))
+            Log.d("already an id", "" + sharedPreferences.getString("id", ""))
         }
 
+        //get location
+         location =  SimpleLocation(this, true,false,1000,true);
+        if (!location!!.hasLocationEnabled()) {
+            Log.d("here","here1")
+            SimpleLocation.openSettings(this);
+        }
+        location!!.setListener {
+                lat = location!!.latitude
+                long = location!!.longitude
+                Log.d("new location",""+ location!!.longitude+" "+location!!.latitude)
+        }
     }
+
+    override fun onResume(){
+        Log.d("here","here3")
+        location?.beginUpdates()
+        lat = location!!.latitude
+        long = location!!.longitude
+        super.onResume()
+
+    }
+    override fun onPause() {
+        Log.d("here","here4")
+        location!!.endUpdates()
+        lat = location!!.latitude
+        long = location!!.longitude
+        super.onPause()
+
+    }
+
+
 }
--- app/src/main/java/ch/friender/Map.kt
+++ app/src/main/java/ch/friender/Map.kt
@@ -1,18 +1,17 @@
 package ch.friender
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.IconFactory
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
@@ -24,6 +23,9 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.maps.Style.OnStyleLoaded
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT
 
 
 class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
@@ -51,6 +53,7 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
         this.mapboxMap = mapboxMap
         mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
             enableLocationComponent(style)
+
             if (mapboxMap.locationComponent.lastKnownLocation != null) {
                 val position = CameraPosition.Builder()
                         .target(LatLng(mapboxMap.locationComponent.lastKnownLocation!!.latitude, mapboxMap.locationComponent.lastKnownLocation!!.longitude))
@@ -60,6 +63,7 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
                 mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000)
             }
         }
+
     }
 
     @SuppressLint("MissingPermission")
