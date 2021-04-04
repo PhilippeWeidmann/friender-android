1baeec147167a7d53592befb8fdb6b614ae93987
MagicMustache <milo.gianinazzi@gmail.com>
2021-04-05 00:55:45 +0200

    proto marker interaction

--- app/src/main/java/ch/friender/Map.kt
+++ app/src/main/java/ch/friender/Map.kt
@@ -6,15 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.annotations.IconFactory
-import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
@@ -29,13 +30,14 @@ import com.mapbox.mapboxsdk.maps.Style.OnStyleLoaded
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT
+import com.mapbox.mapboxsdk.utils.BitmapUtils
 
 
 class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
     private var permissionsManager: PermissionsManager? = null
     private lateinit var mapboxMap: MapboxMap
     private lateinit var mapView: MapView
-
+    private val ICON_MARKER:String = "basic-marker"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(requireActivity(), getString(R.string.mapbox_access_token))
@@ -51,7 +53,7 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
 
         var bottomSheet = view.findViewById<ConstraintLayout>(R.id.bottom_sheet)
         var bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
 
         return view
@@ -61,7 +63,7 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
         this.mapboxMap = mapboxMap
         mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
             enableLocationComponent(style)
-
+            mapboxMap.getStyle(this::addMarkerImageToStyle)
             if (mapboxMap.locationComponent.lastKnownLocation != null) {
                 val position = CameraPosition.Builder()
                         .target(LatLng(mapboxMap.locationComponent.lastKnownLocation!!.latitude, mapboxMap.locationComponent.lastKnownLocation!!.longitude))
@@ -70,10 +72,47 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
                         .build()
                 mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000)
             }
+
+            addMarkerImageToStyle(style)
+            // create symbol manager object
+            val symbolManager = SymbolManager(mapView, mapboxMap, style)
+            val symbol = symbolManager.create(SymbolOptions()
+                    .withLatLng(LatLng(46.2, 6.1667))
+                    .withIconImage(ICON_MARKER)
+                    .withIconSize(1.0f))
+            // add click listeners if desired
+            symbolManager.addClickListener {
+                val bottomSheet = requireView().findViewById<ConstraintLayout>(R.id.bottom_sheet)
+                val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+                if(bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED){
+                    collapseSheet()
+                }else{
+                    expandSheet()
+                }
+                true
+            }
+            // set non-data-driven properties, such as:
+            symbolManager.iconAllowOverlap = true
+            symbolManager.iconTranslate = arrayOf(-4f, 5f)
+            symbolManager.iconRotationAlignment = ICON_ROTATION_ALIGNMENT_VIEWPORT
+
+            mapboxMap.addOnMapClickListener {
+                val bottomSheet = requireView().findViewById<ConstraintLayout>(R.id.bottom_sheet)
+                val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+                if(bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED){
+                    collapseSheet()
+                }else{
+                    expandSheet()
+                }
+                true
+            }
         }
-
     }
-
+    private fun addMarkerImageToStyle(style: Style) {
+        style.addImage(ICON_MARKER,
+                BitmapUtils.getBitmapFromDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.mapbox_marker_icon_20px_blue))!!,
+                false)
+    }
     @SuppressLint("MissingPermission")
     private fun enableLocationComponent(loadedMapStyle: Style) {
         // Check if permissions are enabled and if not request
@@ -156,5 +195,17 @@ class Map : Fragment(), OnMapReadyCallback, PermissionsListener {
         fun newInstance(): Map? {
             return Map()
         }
+    }
+    private fun expandSheet(){
+        var bottomSheet = requireView().findViewById<ConstraintLayout>(R.id.bottom_sheet)
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+    }
+    private fun collapseSheet(){
+        var bottomSheet = requireView().findViewById<ConstraintLayout>(R.id.bottom_sheet)
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
     }
 }
