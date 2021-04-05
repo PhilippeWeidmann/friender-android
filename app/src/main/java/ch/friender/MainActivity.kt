94af4269f5ef2afa95271a8635c27143026953e3
Philippe Weidmann <philweidmann@me.com>
2021-04-05 11:38:21 +0200

    Refactor networking
    Signed-off-by: Philippe Weidmann <philweidmann@me.com>

--- app/src/main/java/ch/friender/MainActivity.kt
+++ app/src/main/java/ch/friender/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import ch.friender.networking.ApiFetcher
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -23,27 +24,29 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiFetcher.initWithContext(this)
+
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
-
-        //get ID
+        
         val sharedPreferences = getPreferences(MODE_PRIVATE)
         val editor = sharedPreferences.edit()
-        val url = "http://10.0.2.2:1337/user/register"
-        val requestQueue = Volley.newRequestQueue(this)
-        if (sharedPreferences.getString("id", "")?.isEmpty() == true) {
-            val getId = JsonObjectRequest(Request.Method.POST, url, null, { res ->
-                val id = res.get("data").toString()
-                Log.d("ID", id)
-                editor.putString("id", id)
-                editor.apply()
-            }, { error ->
-                Log.e("error on getting id", error.toString())
-            })
-            requestQueue.add(getId)
+        if (sharedPreferences.getInt("id", -1) == -1) {
+            ApiFetcher.registerUser { userId, error ->
+
+                userId?.let {
+                    editor.putInt("id", it)
+                    editor.apply()
+                }
+
+                error?.let {
+                    Log.e("error on getting id", error.toString())
+                }
+            }
         } else {
-            Log.d("already an id", "" + sharedPreferences.getString("id", ""))
+            Log.d("already an id", "" + sharedPreferences.getInt("id", -1))
         }
 
         //get location
@@ -84,5 +87,5 @@ class MainActivity : FragmentActivity() {
         super.onPause()
 
     }
-    
+
 }
---
+++ app/src/main/java/ch/friender/networking/ApiFetcher.kt
@@ -0,0 +1,31 @@
+package ch.friender.networking
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+
+object ApiFetcher {
+
+    private const val baseUrl = "http://10.0.2.2:1337/"
+
+    lateinit var requestQueue: RequestQueue
+
+    fun initWithContext(context: Context) {
+        requestQueue = Volley.newRequestQueue(context)
+    }
+
+    fun registerUser(completion: (Int?, VolleyError?) -> Unit) {
+        val registerUrl = baseUrl + "user/register"
+        val request = JsonObjectRequest(Request.Method.POST, registerUrl, null, { res ->
+            completion(res.getJSONObject("data").getInt("id"), null)
+        }, { error ->
+            completion(null, error)
+        })
+        requestQueue.add(request)
+    }
+
+}
